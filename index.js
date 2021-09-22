ae25b4188f5e762f2a052a6cd5a19be473a63ae0
odion100 <odionedwards@gmail.com>
2021-09-22 19:59:10 -0400

    added dispatcher to the index file

--- index.js
+++ index.js
@@ -2,10 +2,12 @@
 const AppFactory = require("./tjs/App/App");
 const ClientFactory = require("./tjs/Client/Client");
 const HttpClientFactory = require("./tjs/HttpClient/HttpClient");
+const DispatcherFactory = require("./tjs/Dispatcher/Dispatcher");
 
 const App = AppFactory();
 const HttpClient = HttpClientFactory();
 const Client = ClientFactory();
+const Dispatcher = DispatcherFactory();
 
 module.exports = {
   //Export these pre-created objects for convenient object destructuring
@@ -13,10 +15,12 @@ module.exports = {
   App,
   HttpClient,
   Client,
+  Dispatcher,
   //export all modules themselves
   //all these modules export factory functions
   //to ensure non-singleton behavior
   AppFactory,
   ClientFactory,
-  HttpClientFactory
+  HttpClientFactory,
+  DispatcherFactory,
 };
